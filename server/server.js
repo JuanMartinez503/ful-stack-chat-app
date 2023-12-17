37260fa1e63e43f292787ddbec686b2c5c684570
Juan Martinez <juanmartinez5678@hotmail.com>
2023-12-17 13:59:08 -0500

    I added the server.js file to run the server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const db = require('./config/connection')
+const routes = require('./routes')
+const PORT = process.env.PORT || 3001
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+if (process.env.NODE_ENV ==='production'){
+app.use(express.static(path.join(__dirname, '../client/dist')))
+}
+app.use(routes)
+
+db.once('open',()=>{
+    app.listen(PORT, console.log(`App is listening at ${PORT}`))
+})
